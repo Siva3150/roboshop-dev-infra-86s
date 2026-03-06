5be6c790e3de9653b2a4dd3f4effb02dee9c8dbd
Siva prasad <sivaprasad3150@gmail.com>
2026-03-06 11:37:32 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -8,6 +8,6 @@
 # ansible-playbook -i inventory main.yaml
 
 #!/bin/bash 
-component = $1 
+component=$1 
 dnf install ansible -y 
 ansible-pull -U https://github.com/Siva3150/ansible-roboshop-roles-tf-86s.git -e component=$component main.yaml
