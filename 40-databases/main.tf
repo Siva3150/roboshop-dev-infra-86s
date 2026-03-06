cc095f8714f03e4ea1df7f5c6657f2894d962d21
Siva prasad <sivaprasad3150@gmail.com>
2026-03-06 11:41:30 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -12,6 +12,7 @@ resource "aws_instance" "mongodb" {
   )
 }
 
+# mongodb
 resource "terraform_data" "mongodb" {
   triggers_replace = [
     aws_instance.mongodb.id 
@@ -38,6 +39,54 @@ resource "terraform_data" "mongodb" {
         "chmod +x /tmp/bootstrap.sh",
         #"sudo sh /tmp/bootstrap.sh"
          "sudo sh /tmp/bootstrap.sh mongodb"
+     ]
+    
+  }
+
+
+}
+
+#redis
+
+resource "aws_instance" "redis" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.redis_sg_id]
+  subnet_id = local.database_subent_ids
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name =  "${local.common_name_suffix}-redis"
+    }
+  )
+}
+resource "terraform_data" "redis" {
+  triggers_replace = [
+    aws_instance.redis.id 
+  ]
+
+#connection block using to connect to server
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.redis.private_ip
+  }
+
+ # terraform copies this file to mongodb server
+  provisioner "file" {
+    
+        source = "bootstrap.sh"
+        destination = "/tmp/bootstrap.sh" 
+     
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+        "chmod +x /tmp/bootstrap.sh",
+        #"sudo sh /tmp/bootstrap.sh"
+         "sudo sh /tmp/bootstrap.sh redis"
      ]
     
   }
