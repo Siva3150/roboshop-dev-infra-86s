8ef1118f033d473a9306cb9208d9efb37716538b
Siva prasad <sivaprasad3150@gmail.com>
2026-03-08 18:50:53 +0530

    terraform

---
+++ 60-catalogue/locals.tf
@@ -0,0 +1,14 @@
+locals{
+  common_name_suffix = "${var.project_name}-${var.environment}" # roboshop-dev
+  ami_id = data.aws_ami.joindevops.id 
+  private_subent_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  private_subent_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
+  catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value 
+  vpc_id = data.aws_ssm_parameter.vpc_id.value 
+  common_tags = {
+        Project = var.project_name
+        Environment = var.environment
+        Terraform = "true"
+    }
+
+}
