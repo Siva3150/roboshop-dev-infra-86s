9e86ec843e8d6441c73b9c6c92766fe72b8b800a
Siva prasad <sivaprasad3150@gmail.com>
2026-03-12 11:17:39 +0530

    terraform

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -1,11 +1,11 @@
-resource "aws_security_group_rule" "frontend_frontend_alb" {
-  type              = "ingress"
-  security_group_id = local.frontend_sg_id
-  source_security_group_id = local.frontend_alb_sg_id
-  from_port = 80
-  to_port = 80
-  protocol = "tcp"
-}
+# resource "aws_security_group_rule" "frontend_frontend_alb" {
+#   type              = "ingress"
+#   security_group_id = local.frontend_sg_id
+#   source_security_group_id = local.frontend_alb_sg_id
+#   from_port = 80
+#   to_port = 80
+#   protocol = "tcp"
+# }
 
 resource "aws_security_group_rule" "frontend_bastion" {
   type              = "ingress"
@@ -98,14 +98,14 @@ resource "aws_security_group_rule" "catalogue_bastion" {
   protocol = "tcp"
 }
 
-resource "aws_security_group_rule" "catalogue_backend_alb" {
-  type              = "ingress"
-  security_group_id = local.catalogue_sg_id
-  source_security_group_id = local.backend_alb_sg_id
-  from_port         = 8080
-  protocol          = "tcp"
-  to_port           = 8080
-}
+# resource "aws_security_group_rule" "catalogue_backend_alb" {
+#   type              = "ingress"
+#   security_group_id = local.catalogue_sg_id
+#   source_security_group_id = local.backend_alb_sg_id
+#   from_port         = 8080
+#   protocol          = "tcp"
+#   to_port           = 8080
+# }
 
 #mongodb 
 resource "aws_security_group_rule" "mongodb_catalogue" {
