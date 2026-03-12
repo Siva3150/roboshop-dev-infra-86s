8d545afa1937e2d165655ebd929259113d9e80b2
Siva prasad <sivaprasad3150@gmail.com>
2026-03-12 12:05:45 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,14 +1,14 @@
-module "components" {
-    source = "../../terraform-roboshop-component-86s"
-    component = var.component
-    rule_priority = var.rule_priority
-  
-}
-
 # module "components" {
-#     for_each = var.components
-#     source = "git::https://github.com/Siva3150/terraform-roboshop-component-86s.git?ref=main"
-#     component = each.key
-#     rule_priority = each.value 
+#     source = "../../terraform-roboshop-component-86s"
+#     component = var.component
+#     rule_priority = var.rule_priority
   
 # }
+
+module "components" {
+    for_each = var.components
+    source = "git::https://github.com/Siva3150/terraform-roboshop-component-86s.git?ref=main"
+    component = each.key
+    rule_priority = each.value 
+  
+}
