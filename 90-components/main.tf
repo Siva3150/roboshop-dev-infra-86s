7070c1f145e4b244801c0e57ee3c1e813338b010
Siva prasad <sivaprasad3150@gmail.com>
2026-03-12 12:03:31 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -4,3 +4,11 @@ module "components" {
     rule_priority = var.rule_priority
   
 }
+
+# module "components" {
+#     for_each = var.components
+#     source = "git::https://github.com/Siva3150/terraform-roboshop-component-86s.git?ref=main"
+#     component = each.key
+#     rule_priority = each.value 
+  
+# }
