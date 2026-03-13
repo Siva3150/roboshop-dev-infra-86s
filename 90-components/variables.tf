71f842ea879a0c4384e583cc46d6b22377ac5799
Siva prasad <sivaprasad3150@gmail.com>
2026-03-13 14:48:16 +0530

    terraform

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -1,11 +1,9 @@
 variable "component" {
     default = "catalogue"
-  
 }
 
 variable "rule_priority" {
     default = 10
-  
 }
 
 variable "components" {
@@ -14,20 +12,19 @@ variable "components" {
             rule_priority = 10
         }
         user = {
-            rule_priority = 20 
+            rule_priority = 20
         }
         cart = {
             rule_priority = 30
         }
         shipping = {
-            rule_priority = 40 
+            rule_priority = 40
         }
         payment = {
             rule_priority = 50
         }
         frontend = {
-            rule_priority = 60 
+            rule_priority = 10
         }
     }
-  
 }
